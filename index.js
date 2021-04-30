e1216cbba20d072c22b2d605e71dfdfa95d97471
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-04-30 16:32:40 +0100

    added boilerplate js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+const writeToFile = (answers) => {};
+
+const getAnswersFromQuestions = async (questions) => {
+  const answers = await inquirer.prompt(questions);
+  return answers;
+};
+
+const init = async () => {
+  const questions = [];
+  const answers = await getAnswersFromQuestions(questions);
+  writeToFile(answers);
+};
+
+init();
