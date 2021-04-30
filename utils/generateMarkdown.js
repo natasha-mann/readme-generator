d4e88a3da38927a9b148572c12dcd6e7a612892c
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-04-30 19:43:11 +0100

    updated markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,26 +12,34 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
-  return `# ${data.title}
+  return `
   # ${data.title} 
+
   ## Description
   ${data.description}
+
   ## Table of Contents
     * [Installation](#installation)
     * [Usage](#usage)
     * [Contributing](#contributing)
     * [Tests](#tests)
     * [License](#license)
+    * 
   ## Installation
   ${data.installation}
+
   ## Usage
   ${data.usage}
+
   ## Contributing
   ${data.contributing}
+
   ## Tests
   ${data.tests}
+
   ## Licence
   This project is licensed under the ${data.licence} license.
+  
   ## Questions
   If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.github}.
 `;
