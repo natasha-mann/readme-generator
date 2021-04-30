7cb16b6d9013b07fd7a2ea9793a4845ee797a74c
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-04-30 20:43:24 +0100

    added function to render license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseBadge = (license) => {
+  return `https://img.shields.io/badge/license-${license}-green`;
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,10 +14,12 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
+  const licenseBadge = renderLicenseBadge(data.license);
+
   return `
   # ${data.title} 
 
-  ![${data.license} badge](https://img.shields.io/badge/license-${data.license}-green)
+  ![${data.license} badge](${licenseBadge})
 
   ## Description
   ${data.description}
