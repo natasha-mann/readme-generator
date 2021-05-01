1bd12f3e9d762f7cb9566e67156ad241a0317c2b
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-01 22:42:18 +0100

    fixed formatting of generated readme

--- index.js
+++ index.js
@@ -13,29 +13,30 @@ const init = async () => {
       name: "title",
     },
     {
-      message: "Please give a description for your project.",
+      message: "Please give a short description for your project",
       name: "description",
     },
     {
-      message: "Please give the installation instructions for your project.",
+      message: "What are the installation instructions for your project?",
       name: "installation",
     },
 
     {
-      message: "Please give the usage information for your project.",
+      message: "Please give the usage information for your project",
       name: "usage",
     },
     {
-      message: "Please give the contribution guidelines for your project.",
+      message: "Please give the contribution guidelines for your project",
       name: "contributing",
     },
     {
-      message: "Please give the test instructions for your project.",
+      message:
+        "If applicable, please give the test instructions for your project",
       name: "tests",
     },
     {
       type: "list",
-      message: "Please choose a licence for your project.",
+      message: "Please choose a licence for your project",
       name: "license",
       choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
     },
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,12 +5,61 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 const renderLicenseBadge = (license) => {
-  return `https://img.shields.io/badge/license-${license}-green`;
+  if (license !== "None") {
+    return `
+![${license} badge](https://img.shields.io/badge/license-${license}-green)
+`;
+  } else {
+    return "";
+  }
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (license) => {
+  if (license !== "None") {
+    return `
+## License
+This project is licensed under the ${license} license.
+`;
+  } else {
+    return "";
+  }
+};
+
+const renderTestsSection = (tests) => {
+  if (tests !== "") {
+    return `
+## Tests
+${tests}
+`;
+  } else {
+    return "";
+  }
+};
+
+const renderTableOfContents = (answers) => {
+  if (answers.license !== "None") {
+    return `
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+`;
+  } else {
+    return `
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+`;
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (answers) => {
@@ -27,23 +76,19 @@ const generateMarkdown = (answers) => {
   } = answers;
 
   const licenseBadge = renderLicenseBadge(license);
+  const licenseSection = renderLicenseSection(license);
+  const tableOfContents = renderTableOfContents(answers);
+  const testsSection = renderTestsSection(tests);
 
   return `
   # ${title} 
 
-  ![${license} badge](${licenseBadge})
+  ${licenseBadge}
 
   ## Description
   ${description}
 
-  ## Table of Contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [License](#license)
-  * [Questions](#questions)
-    
+  ${tableOfContents}  
 
   ## Installation
 
@@ -55,11 +100,9 @@ const generateMarkdown = (answers) => {
   ## Contributing
   ${contributing}
 
-  ## Tests
-  ${tests}
+  ${testsSection}
 
-  ## License
-  This project is licensed under the ${license} license.
+  ${licenseSection}
 
   ## Questions
   If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at [Github](${github}).
